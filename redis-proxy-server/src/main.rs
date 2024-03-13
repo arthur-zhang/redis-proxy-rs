f12cab781f56f9285b280f0f7a64c7f87e650a75
arthur-zhang <zhangya_no1@qq.com>
2024-03-13 10:48:33 +0800

    add prometheus conf

--- redis-proxy-server/src/main.rs
+++ redis-proxy-server/src/main.rs
@@ -2,7 +2,9 @@ use std::env::args;
 use std::path::Path;
 use std::sync::Arc;
 
-use log::{debug, info};
+use log::{info};
+#[cfg(not(target_env = "msvc"))]
+use tikv_jemallocator::Jemalloc;
 
 use redis_proxy::config::Config;
 use redis_proxy::prometheus::PrometheusServer;
@@ -21,27 +23,27 @@ mod filter_trait;
 mod mirror_filter;
 mod log_filter;
 mod blacklist_filter;
-#[cfg(not(target_env = "msvc"))]
-use tikv_jemallocator::Jemalloc;
 
 #[cfg(not(target_env = "msvc"))]
 #[global_allocator]
 static GLOBAL: Jemalloc = Jemalloc;
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     env_logger::init();
 
-    PrometheusServer::start("127.0.0.1:9095", "/metrics");
     let conf_path = args().nth(1).ok_or(anyhow::anyhow!("config file path is required"))?;
     let conf = get_conf(conf_path.as_ref()).map_err(|e| { anyhow::anyhow!("load config error: {:?}", e) })?;
-    debug!("config: {:?}", conf);
-    let conf = Arc::new(conf);
-    if conf.debug.is_some_and(|it| it) {
+    info!("loaded config: {:?}", conf);
+    start_prometheus_server(&conf);
+
+    if conf.debug.unwrap_or(false) {
         console_subscriber::init();
     }
 
     info!("starting redis proxy server...");
 
+    let conf = Arc::new(conf);
     let filters = load_filters(&conf);
 
     let proxy = RedisProxyImpl { filters, conf: conf.clone() };
@@ -51,21 +53,28 @@ async fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
+fn start_prometheus_server(conf: &Config) {
+    if let Some(ref prometheus) = conf.prometheus {
+        PrometheusServer::start(&prometheus.address, &prometheus.export_uri);
+        info!("start prometheus server at: {}{}", prometheus.address, prometheus.export_uri);
+    }
+}
+
 fn load_filters(conf: &Arc<Config>) -> Vec<Box<dyn Proxy<CTX=FilterContext> + Send + Sync>> {
     let mut filters: Vec<Box<dyn Proxy<CTX=FilterContext> + Send + Sync>> = vec![];
 
     if let Some(ref blacklist_filter) = conf.filter_chain.blacklist {
         let blacklist_filter = BlackListFilter::new(blacklist_filter.block_patterns.clone(), &blacklist_filter.split_regex).unwrap();
-        // filters.push(Box::new(blacklist_filter));
+        filters.push(Box::new(blacklist_filter));
     }
 
     if let Some(ref mirror) = conf.filter_chain.mirror {
         let mirror_filter = Mirror::new(&mirror.address, &mirror.mirror_patterns, &mirror.split_regex, mirror.queue_size).unwrap();
-        // filters.push(Box::new(mirror_filter));
+        filters.push(Box::new(mirror_filter));
     }
     if let Some(_) = conf.filter_chain.log {
         let log_filter = LogFilter {};
-        // filters.push(Box::new(log_filter));
+        filters.push(Box::new(log_filter));
     }
     filters
 }
