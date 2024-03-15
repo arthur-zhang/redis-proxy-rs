ed1fa761b507e7a8904854c79298c11f3e10f8a9
tangzhenhuang <tangzhenhuang@cvte.com>
2024-03-15 15:15:25 +0800

    [modify]添加bandwidth指标数据

--- redis-proxy/src/prometheus.rs
+++ redis-proxy/src/prometheus.rs
@@ -2,13 +2,19 @@ use std::net::SocketAddr;
 
 use lazy_static::lazy_static;
 use prometheus_exporter::Builder;
-use prometheus_exporter::prometheus::core::{AtomicI64, GenericGaugeVec};
-use prometheus_exporter::prometheus::register_histogram_vec_with_registry;
+use prometheus_exporter::prometheus::{HistogramVec, register_histogram_vec_with_registry, register_int_counter_vec_with_registry};
+use prometheus_exporter::prometheus::core::{AtomicI64, AtomicU64, GenericCounterVec, GenericGaugeVec};
 use prometheus_exporter::prometheus::register_int_gauge_vec_with_registry;
 use prometheus_exporter::prometheus::Registry;
 
 pub const CONN_DOWNSTREAM: &str = "downstream";
 pub const CONN_UPSTREAM: &str = "upstream";
+
+pub const TRAFFIC_TYPE_INGRESS: &str = "ingress";
+pub const TRAFFIC_TYPE_EGRESS: &str = "egress";
+
+pub const RESP_SUCCESS: &str = "success";
+pub const RESP_FAILED: &str = "failed";
 
 lazy_static! {
     pub static ref REGISTRY: prometheus_exporter::prometheus::Registry =
@@ -30,7 +36,8 @@ impl PrometheusServer {
 
 pub struct Metrics {
     pub connections: GenericGaugeVec<AtomicI64>,
-    pub request_latency: prometheus_exporter::prometheus::HistogramVec,
+    pub request_latency: HistogramVec,
+    pub bandwidth: GenericCounterVec<AtomicU64>,
 }
 
 impl Metrics {
@@ -42,12 +49,18 @@ impl Metrics {
 
         let request_latency = register_histogram_vec_with_registry!(
             "request_latency", "The latency of the requests.",
-            &["cmd_type"], vec![0.001, 0.002, 0.005, 0.01, 0.1, 1.0, 10.0], registry
+            &["cmd", "status"], vec![0.001, 0.002, 0.005, 0.01, 0.1, 1.0, 10.0], registry
         ).expect("request_latency metrics can be created");
 
+        let bandwidth = register_int_counter_vec_with_registry!(
+            "bandwidth", "The bandwidth of the requests.",
+            &["cmd", "type"], registry
+        ).expect("bandwidth metrics can be created");
+        
         Self {
             connections,
-            request_latency
+            request_latency,
+            bandwidth
         }
     }
 }
