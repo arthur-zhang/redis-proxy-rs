f08aef9056c680f5825a171249d5feeb258f2666
arthur-zhang <zhangya_no1@qq.com>
2024-03-20 18:03:32 +0800

    optimize

--- redis-proxy-common/src/lib.rs
+++ redis-proxy-common/src/lib.rs
@@ -14,44 +14,40 @@ pub struct ReqPkt {
 
 impl ReqPkt {
     pub fn new(bulk_args: Vec<Bytes>, bytes_total: usize) -> Self {
-        let cmd = unsafe { 
-            std::str::from_utf8_unchecked(&bulk_args[0]) 
-        };
-        let mut cmd_type: SmolStr = cmd.into();
+        let mut cmd_type = bulk_args[0].iter().map(|it| it.to_ascii_lowercase() as char).collect::<SmolStr>();
         if MULTIPART_COMMANDS.contains_key(&cmd_type) && bulk_args.len() > 1 {
-            let sub_cmd = unsafe {
-                std::str::from_utf8_unchecked(&bulk_args[1])
-            };
-            cmd_type = SmolStr::from_iter(cmd_type.chars().chain([' ' as char]).chain(sub_cmd.chars()));
+            cmd_type = cmd_type.chars()
+                .chain([' ' as char])
+                .chain(bulk_args[1].iter().map(|it| it.to_ascii_lowercase() as char))
+                .collect();
         }
         return ReqPkt {
             cmd_type,
             bulk_args,
-            bytes_total
-        }
+            bytes_total,
+        };
     }
-    
+
     pub fn cmd_type(&self) -> &SmolStr {
         &self.cmd_type
     }
-    
-    pub fn keys(&self)-> Option<Vec<&[u8]>> {
-        
+
+    pub fn keys(&self) -> Option<Vec<&[u8]>> {
         None
     }
 }
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_smol_concat() {
-        let a: SmolStr = "hello".into();
-        let b= "world";
-        let c = format!("{} {}", a, b);
+        let a: SmolStr = "HELLO".chars().map(|it| (it as u8).to_ascii_lowercase() as char).collect();
+        let b = "WORLD";
+        let c = format!("{} {}", a, b.to_ascii_lowercase());
         assert_eq!(c, "hello world");
-        let c = a.chars().chain([' ' as char]).chain(b.chars());
-        let d  = SmolStr::from_iter(c);
-        assert_eq!(d, "hello world");
+        let c: SmolStr = a.chars().chain([' ' as char]).chain(b.chars().map(|it| (it as u8).to_ascii_lowercase() as char)).collect();
+        assert_eq!(c, "hello world");
     }
 }
